1647e3ad6f88d776251d8e5c6bb8f49c2b05777d
Tomat <xxlennygamerxx@gmail.com>
2023-02-12 03:07:23 -0800

    win_ implied.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![no_std]
+
 use winapi::shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID, TRUE};
 use winapi::shared::windef::RECT;
 
@@ -12,12 +14,12 @@ pub unsafe extern "system" fn DllMain(
 }
 
 #[no_mangle]
-pub extern "C" fn display_mouse_lock(win_x: f64, win_y: f64, win_width: f64, win_height: f64) {
+pub extern "C" fn display_mouse_lock(x: f64, y: f64, width: f64, height: f64) {
     let mut rect = RECT {
-        left: win_x as i32,
-        top: win_y as i32,
-        right: (win_x + win_width) as i32,
-        bottom: (win_y + win_height) as i32,
+        left: x as i32,
+        top: y as i32,
+        right: (x + width) as i32,
+        bottom: (y + height) as i32,
     };
 
     // so sad unsafe
@@ -30,7 +32,7 @@ pub extern "C" fn display_mouse_lock(win_x: f64, win_y: f64, win_width: f64, win
 pub extern "C" fn display_mouse_unlock() {
     // so sad unsafe
     unsafe {
-        winapi::um::winuser::ClipCursor(std::ptr::null_mut());
+        winapi::um::winuser::ClipCursor(0 as *const RECT);
     }
 }
 
